d435aadccfcc848e012b43d918edf3b516cd09bc
mehmettilkicik <pbmemedo@gmail.com>
2023-05-25 18:29:43 +0300

    controller import

--- src/controllers/contacts.controller.ts
+++ src/controllers/contacts.controller.ts
@@ -1,5 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { GetAllContactsUsecase } from 'src/usecases/contacts/getAllContacts.usecase';
+import { GetContactsByNameUsecase } from 'src/usecases/contacts/getContactsByName.usecase';
+import { GetContactsByNumberUsecase } from 'src/usecases/contacts/getContactsByNumber.usecase';
+import { UpdateContactsNumberUsecase } from 'src/usecases/contacts/updateContactsNumber.usecase';
+import { CreateContactsUsecase } from 'src/usecases/contacts/createContacts.usecase';
+import { DeleteContactsUsecase } from 'src/usecases/contacts/deleteContacts.usecase';
+
 
 @Controller('contacts')
 export class ContactsController {
