97c074d7c2a2e2e87c8759a3813bc8eb0338bec4
mehmettilkicik <pbmemedo@gmail.com>
2023-05-26 11:04:39 +0300

    module update

--- src/usecases/contacts/contacts.module.ts
+++ src/usecases/contacts/contacts.module.ts
@@ -6,10 +6,19 @@ import { GetContactsByNameUsecase } from './getContactsByName.usecase';
 import { GetContactsByNumberUsecase } from './getContactsByNumber.usecase';
 import { UpdateContactsNumberUsecase } from './updateContactsNumber.usecase';
 import { ContactsController } from 'src/controllers/contacts.controller';
+import { ContactsRepository } from 'src/core/repositories/contacts.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contacts } from 'src/core/repositories/entities/contacts.entity';
+
+const ENTITIES = {
+  SQLITE: [Contacts]
+}
 
 @Module({
-  providers: [CreateContactsUsecase, DeleteContactsUsecase, GetAllContactsUsecase,
+    imports:[TypeOrmModule.forFeature(ENTITIES.SQLITE)],
+    providers: [CreateContactsUsecase, DeleteContactsUsecase, GetAllContactsUsecase,
      GetContactsByNameUsecase, GetContactsByNumberUsecase, UpdateContactsNumberUsecase],
-     controllers: [ContactsController]  
+    controllers: [ContactsController],  
+    exports:[ContactsRepository]
 })
 export class ContactsModule {}
