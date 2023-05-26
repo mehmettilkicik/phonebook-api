d951dac6445b62a782b7f2768b9c5b9144657ad7
mehmettilkicik <pbmemedo@gmail.com>
2023-05-26 10:33:53 +0300

    controller

--- src/controllers/contacts.controller.ts
+++ src/controllers/contacts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query, ValidationPipe } from '@nestjs/common';
 import { GetAllContactsUsecase } from 'src/usecases/contacts/getAllContacts.usecase';
 import { GetContactsByNameUsecase } from 'src/usecases/contacts/getContactsByName.usecase';
 import { GetContactsByNumberUsecase } from 'src/usecases/contacts/getContactsByNumber.usecase';
@@ -9,4 +9,26 @@ import { DeleteContactsUsecase } from 'src/usecases/contacts/deleteContacts.usec
 
 @Controller('contacts')
 export class ContactsController {
+    constructor(
+        private readonly getAllContactsUsecase : GetAllContactsUsecase,
+        private readonly getContactsByNameUsecase : GetContactsByNameUsecase,
+        private readonly getContactsByNumberUsecase : GetContactsByNumberUsecase,
+        private readonly updateContactsNumberUsecase : UpdateContactsNumberUsecase,
+        private readonly createContactsUsecase : CreateContactsUsecase,
+        private readonly deleteContactsUsecase : DeleteContactsUsecase
+    ){}
+
+    @Get('contacts')
+    async getContacts(){
+        const contacts = await this.getAllContactsUsecase.execute();
+        return contacts;
+    }
+
+    @Get('contact')
+    async getContactByName(@Query('name',ValidationPipe) name: string){
+        const contact = await this.getContactsByNameUsecase.execute(name);
+        return contact;
+    }
+
+    
 }
