74a5fe8a32070b2fc5ea14908033beb210c4238e
mehmettilkicik <pbmemedo@gmail.com>
2023-05-26 10:53:38 +0300

    controller finished

--- src/controllers/contacts.controller.ts
+++ src/controllers/contacts.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, ParseIntPipe, Query, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query, ValidationPipe } from '@nestjs/common';
 import { GetAllContactsUsecase } from 'src/usecases/contacts/getAllContacts.usecase';
 import { GetContactsByNameUsecase } from 'src/usecases/contacts/getContactsByName.usecase';
 import { GetContactsByNumberUsecase } from 'src/usecases/contacts/getContactsByNumber.usecase';
 import { UpdateContactsNumberUsecase } from 'src/usecases/contacts/updateContactsNumber.usecase';
 import { CreateContactsUsecase } from 'src/usecases/contacts/createContacts.usecase';
 import { DeleteContactsUsecase } from 'src/usecases/contacts/deleteContacts.usecase';
+import { uptime } from 'process';
+import { AddContactsDto, UpdateContactsDto} from 'src/core/dtos/Contacts.dto';
 
 
 @Controller('contacts')
@@ -25,10 +27,34 @@ export class ContactsController {
     }
 
     @Get('contact')
-    async getContactByName(@Query('name',ValidationPipe) name: string){
+    async getContactsByName(@Query('name',ValidationPipe) name: string){
         const contact = await this.getContactsByNameUsecase.execute(name);
         return contact;
     }
 
-    
+    @Get('contact')
+    async getContactsByNumber(@Query('phoneNumber',ValidationPipe) phoneNumber: string){
+        const contact = await this.getContactsByNumberUsecase.execute(phoneNumber);
+        return contact;
+    }
+
+    @Put('contact')
+    async updateContacts(@Body() updateContactsDto:UpdateContactsDto){
+        const {name, phoneNumber} = updateContactsDto;
+        await this.updateContactsNumberUsecase.execute(name,phoneNumber);
+        return 'success';
+    }
+
+    @Delete('contact:phoneNumber')
+    async deleteContacts(@Param('phoneNumber') phoneNumber: string){
+    await this.deleteContactsUsecase.execute(phoneNumber);
+    return 'success';   
+    }
+
+    @Post('contact')
+    async addContacs(@Body() addContactsDto:AddContactsDto){
+        const {name,phoneNumber} = addContactsDto;
+        await this.createContactsUsecase.execute(name,phoneNumber);
+        return 'success';
+    }
 }
--- src/core/dtos/Contacts.dto.ts
+++ src/core/dtos/Contacts.dto.ts
@@ -1,13 +1,13 @@
 import { IsNotEmpty, IsString } from "class-validator";
 
-export class addContactsDto{
+export class AddContactsDto{
     @IsNotEmpty()
     @IsString()
     readonly name: string;
     readonly phoneNumber: string;
 }
 
-export class updateContactsDto{
+export class UpdateContactsDto{
     @IsNotEmpty()
     @IsString()
     readonly name : string;
